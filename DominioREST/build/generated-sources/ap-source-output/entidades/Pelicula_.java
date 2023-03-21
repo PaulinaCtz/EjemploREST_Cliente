520b5c1ff14622f96c3c8b03766d9150b50b5925
Alfredo Valenzuela <jaime.valenzuela228249@potros.itson.edu.mx>
2023-03-21 15:26:03 -0700

    Quitar el dto

---
+++ DominioREST/build/generated-sources/ap-source-output/entidades/Pelicula_.java
@@ -0,0 +1,16 @@
+package entidades;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-03-21T15:24:22", comments="EclipseLink-2.7.10.v20211216-rNA")
+@StaticMetamodel(Pelicula.class)
+public class Pelicula_ { 
+
+    public static volatile SingularAttribute<Pelicula, String> director;
+    public static volatile SingularAttribute<Pelicula, Long> id;
+    public static volatile SingularAttribute<Pelicula, String> nombre;
+    public static volatile SingularAttribute<Pelicula, String> sinopsis;
+
+}
